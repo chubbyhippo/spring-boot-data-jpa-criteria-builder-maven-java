3ce8639f4746fc81b0989d637d3bda315dc95b58
ch <mkmsacc@outlook.com>
2023-04-06 22:59:36 +0700

    Add student

---
+++ src/main/java/com/example/demo/entity/Student.java
@@ -0,0 +1,28 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+    @Id
+    private Long id;
+    private String firstname;
+    private String lastname;
+
+    @ManyToMany
+    @JoinTable(
+            name = "course_student",
+            joinColumns = @JoinColumn(name = "student_id"),
+            inverseJoinColumns = @JoinColumn(name = "course_id"))
+    private Set<Course> courses;
+}
